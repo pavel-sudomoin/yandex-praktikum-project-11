f179c4457c235302166d9242804f446b8943c8b9
Pavel Sudomoin <sudomoin.pavel@yandex.ru>
2020-02-23 22:55:53 +0300

    install babel

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,12 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
-    }
+    },
+    module: {
+        rules: [{ // тут описываются правила
+            test: /\.js$/, // регулярное выражение, которое ищет все js файлы
+            use: { loader: "babel-loader" }, // весь JS обрабатывается пакетом babel-loader
+            exclude: /node_modules/ // исключает папку node_modules
+            }]
+        }
 }
