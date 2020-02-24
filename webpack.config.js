75b297172c405dc292e23c0b5a1bb933ef6e99f3
Pavel Sudomoin <sudomoin.pavel@yandex.ru>
2020-02-24 15:25:21 +0300

    add style-loader and optimize-css-assets-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
+const path = require('path');
+const webpack = require('webpack');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
-const webpack = require('webpack');
-
-const path = require('path');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
     entry: { main: './src/js/script.js' },
@@ -20,7 +21,7 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+                use: [(isDev ? 'style-loader' : MiniCssExtractPlugin.loader), 'css-loader', 'postcss-loader']
             },
             {
                 test: /\.(png|jpg|gif|ico|svg)$/,
@@ -41,6 +42,14 @@ module.exports = {
     plugins: [
         new MiniCssExtractPlugin({
             filename: 'style.[contenthash].css',
+        }),
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorPluginOptions: {
+                    preset: ['default'],
+            },
+            canPrint: true
         }),
         new HtmlWebpackPlugin({
             inject: false,
